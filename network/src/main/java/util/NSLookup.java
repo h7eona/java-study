71c4e3c323728d7bddb9790052a0d512d5f77c11
h7eona <jessie117@naver.com>
2024-11-27 17:25:01 +0900

    02. TCP소켓프로그래1

---
+++ network/src/main/java/util/NSLookup.java
@@ -0,0 +1,31 @@
+package util;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class NSLookup {
+
+	public static void main(String[] args) {
+		while (true) {
+			Scanner scanner = new Scanner(System.in);
+			
+			try {
+				String domain = scanner.nextLine();
+				
+				if ("exit".equals(domain)) {
+					break;
+				}
+				
+				InetAddress[] InetAddresses = InetAddress.getAllByName(domain);
+				
+				for(InetAddress inetAddress : InetAddresses) {
+					System.out.println(inetAddress);
+				}
+	
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
